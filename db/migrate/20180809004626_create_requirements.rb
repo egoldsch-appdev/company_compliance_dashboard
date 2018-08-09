2ea2ce0e4c4c164ef2346d234d388fe12ffcb6ae
Raghu Betina <raghu@firstdraft.com>
2018-08-09 00:46:26 +0000

    Generate requirements resource with starter:resource

---
+++ db/migrate/20180809004626_create_requirements.rb
@@ -0,0 +1,13 @@
+class CreateRequirements < ActiveRecord::Migration
+  def change
+    create_table :requirements do |t|
+      t.integer :employee_id
+      t.integer :department_id
+      t.integer :region_id
+      t.integer :elearning_id
+
+      t.timestamps
+
+    end
+  end
+end
